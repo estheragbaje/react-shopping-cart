0bb5f758c689f93ca51df32a8abdc486d064d5b5
Folasade Agbaje <folasadeagbaje@gmail.com>
2019-10-07 13:32:31 +0100

    Consume data using the useContext hook from ProductContext and CartContext

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Route } from "react-router-dom";
 import data from "./data";
 import ProductContext from "./contexts/ProductContext";
+import CartContext from "./contexts/CartContext";
 
 // Components
 import Navigation from "./components/Navigation";
@@ -18,19 +19,20 @@ function App() {
 
   return (
     <div className="App">
-      <Navigation cart={cart} />
-
-      {/* Routes */}
       <ProductContext.Provider value={{ products, addItem }}>
-        {/* <Route
+        <CartContext.Provider value={{ cart }}>
+          <Navigation cart={cart} />
+          {/* Routes */}
+          {/* <Route
           exact
           path="/"
           render={() => <Products products={products} addItem={addItem} />}
         /> */}
-        <Route exact path="/" component={Products} />
+          <Route exact path="/" component={Products} />
+          {/* <Route path="/cart" render={() => <ShoppingCart cart={cart} />} /> */}
+          <Route path="/cart" component={cart} />} />
+        </CartContext.Provider>
       </ProductContext.Provider>
-
-      <Route path="/cart" render={() => <ShoppingCart cart={cart} />} />
     </div>
   );
 }
